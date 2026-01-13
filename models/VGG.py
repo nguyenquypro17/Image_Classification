a3723011338cc98e9f49319623872b757c1a3e36
nguyenquypro17 <nguyenducquy10072005@gmail.com>
2026-01-13 09:41:35 +0700

    redundant

--- models/VGG.py
+++ models/VGG.py
@@ -1,18 +1,6 @@
 import torch 
 from torch import nn
 
-def weight_init(m):
-        if isinstance(m, nn.Conv2d):
-            nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            if m.bias is not None:
-                nn.init.constant_(m.bias, 0)
-        elif isinstance(m, nn.BatchNorm2d):
-            nn.init.constant_(m.weight, 1)
-            nn.init.constant_(m.bias, 0)
-        elif isinstance(m, nn.Linear):
-            nn.init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='relu')
-            nn.init.constant_(m.bias, 0) 
-
 class VGG16(nn.Module):
     def __init__(self):
         super(VGG16, self).__init__()
